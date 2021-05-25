341bec4889faf2f50b42f661a3d0136b92ab8382
João Jerónimo <joao@uniaolisboa-cgtp.pt>
2021-05-25 13:01:05 +0100

    Verificar primeiro se o ZFS root pool está montado...

--- bkhelpers.py
+++ bkhelpers.py
@@ -1,17 +1,19 @@
+import pdb
 
 def get_mounted_fss(c):
-    df_res = c.run("LC_ALL=C df -h")
+    df_res = c.run("LC_ALL=C cat /proc/mounts")
     if df_res.failed:
-        raise bkexceptions.VerificationFailed("Could not verify if ZFS is mounted.")
-    ### Process df output to see of the ZFS pool is there:
+        raise bkexceptions.VerificationFailed("Could not get list of mounted filesystems.")
     # First metadata:
-    df_lines = [ ln.strip() for ln in df_res.stdout.split('\n') ]
-    df_header_line = df_lines[0]
-    df_fs_lines = df_lines[0]
-    # Now, normal output:
-    return [{
-        
-        } for oneline in df_fs_lines]
-    
-    
-    
+    fss_table = [ ln.split() for ln in df_res.stdout.split('\n') ]
+    MOUNTS_COLNAMES = [
+        'fs_spec',
+        'fs_file',
+        'fs_vfstype',
+        'fs_mntops',
+        'fs_freq',
+        'fs_passno',    ]
+    fss_dictionaries = [
+            dict([ (MOUNTS_COLNAMES[idx], ln[idx]) for idx in range(len(MOUNTS_COLNAMES)) ])
+        for ln in fss_table if len(ln)==len(MOUNTS_COLNAMES) ]
+    return fss_dictionaries
--- tasks.py
+++ tasks.py
@@ -13,9 +13,18 @@ import pdb
 
 @task
 def mount_zfs_root(c):
-    all_mounted_fss = mkhelpers.get_mounted_fss(c)
-    #
-    pdb.set_trace()
+    all_mounted_fss = bkhelpers.get_mounted_fss(c)
+    zfs_mounteds = [ fss for fss in all_mounted_fss
+        if fss['fs_vfstype']=='zfs' ]
+    matching_mounteds = [ zfss for zfss in zfs_mounteds
+        if zfss['fs_spec']==bkconfigs.ZFS_ROOT_POOLNAME and zfss['fs_file']==bkconfigs.ZFS_ROOT_MOUNTPOINT ]
+    if len(matching_mounteds)>0:
+        print("ZFS root pool mounted ok. Matching mount entries:")
+        for matching in matching_mounteds:
+            print("== Matching ZFS root pool:")
+            print(matching)
+    else:
+        raise bkexceptions.VerificationFailed("Looks like ZFS root vol (%s, %s) is not mounted." % ( bkconfigs.ZFS_ROOT_POOLNAME, bkconfigs.ZFS_ROOT_MOUNTPOINT ))
 
 @task(pre=[mount_zfs_root])
 def update_backup_rsync(c):
