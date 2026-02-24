5a0d39d7a20bcdb4b40bc87334935f68b36139dd
João Jerónimo <joao.jeronimo.pro@gmail.com>
2026-02-24 10:44:26 +0000

    Cenas 1.

--- test_audit_folders.py
+++ test_audit_folders.py
@@ -1,14 +1,18 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
-import unittest
+import unittest, subprocess
 
 class TestAuditFolders(unittest.TestCase):
     """
-    Tests the audit_folders.bash script.
+    Tests the audit_folders.py script.
     """
     
-    def test_audit_folders(self):
+    def test_audit_folders_lists_everything(self):
+        """
+        The audit_folders.py script lists every file found.
+        """
         self.assertTrue(False)
+        subprocess
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
