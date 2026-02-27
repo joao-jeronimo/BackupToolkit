361539451d7df6903fcd949ce80dab0488b8d69c
João Jerónimo <joao.jeronimo.pro@gmail.com>
2026-02-27 10:46:25 +0000

    Testar que o script permite omitir os OKs.

--- test_audit_folders.py
+++ test_audit_folders.py
@@ -42,6 +42,38 @@ class TestAuditFolders(unittest.TestCase):
             "./test_fixtures/test_audit_folders/folder2/folder4/as_mulheres.txt !!! Sensitive name fragment 'mulher'.",
             "./test_fixtures/test_audit_folders/folder2/folder4/as_sofiaes.txt  !!! Sensitive name fragment 'sofia'.",
             ])
+    
+    def test_audit_folders_omits_oks(self):
+        """
+        OK files can be omited via the «--noscan ok» flag.
+        """
+        prog_run = subprocess.run([ './audit_folders.py', '--noscan', 'ok', './test_fixtures/test_audit_folders' ],
+            check=True, stdout=subprocess.PIPE)
+        splotten_stdout = [ l for l in str(prog_run.stdout, 'utf-8').split('\n') if len(l)>0 ]
+        self.assertEqual(splotten_stdout, [
+            "./test_fixtures/test_audit_folders/folder1/Mountpoints.zip         !!! Sensitive name fragment 'mountpoint'.",
+            "./test_fixtures/test_audit_folders/folder1/bababakeybobobo         !!! Sensitive name fragment 'key'.",
+            "./test_fixtures/test_audit_folders/folder1/file.key                !!! Sensitive extension 'key'.",
+            "./test_fixtures/test_audit_folders/folder1/file.pem                !!! Sensitive extension 'pem'.",
+            "./test_fixtures/test_audit_folders/folder1/file.ppk                !!! Sensitive extension 'ppk'.",
+            "./test_fixtures/test_audit_folders/folder1/id_ed25519              !!! Sensitive name fragment 'id_ed25519'.",
+            "./test_fixtures/test_audit_folders/folder1/id_rsa                  !!! Sensitive name fragment 'id_rsa'.",
+            "./test_fixtures/test_audit_folders/folder2/backup_colornote_mybake !!! Sensitive name fragment 'colornote'.",
+            "./test_fixtures/test_audit_folders/folder2/file.p12                !!! Sensitive extension 'p12'.",
+            "./test_fixtures/test_audit_folders/folder2/folder3/mps.zip         OK",
+            "    > mps/fla/Mountpoints.zip                                          !!! Sensitive name fragment 'mountpoint'.",
+            "    > mps/fla/bababakeybobobo                                          !!! Sensitive name fragment 'key'.",
+            "    > mps/fla/file.key                                                 !!! Sensitive extension 'key'.",
+            "    > mps/fla/file.pem                                                 !!! Sensitive extension 'pem'.",
+            "    > mps/fla/file.ppk                                                 !!! Sensitive extension 'ppk'.",
+            "    > mps/fla/id_ed25519                                               !!! Sensitive name fragment 'id_ed25519'.",
+            "    > mps/fla/id_rsa                                                   !!! Sensitive name fragment 'id_rsa'.",
+            "    > mps/flb/backup_colornote_mybake                                  !!! Sensitive name fragment 'colornote'.",
+            "    > mps/flb/file.p12                                                 !!! Sensitive extension 'p12'.",
+            "./test_fixtures/test_audit_folders/folder2/folder4/as_martaes.txt  !!! Sensitive name fragment 'marta'.",
+            "./test_fixtures/test_audit_folders/folder2/folder4/as_mulheres.txt !!! Sensitive name fragment 'mulher'.",
+            "./test_fixtures/test_audit_folders/folder2/folder4/as_sofiaes.txt  !!! Sensitive name fragment 'sofia'.",
+            ])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
